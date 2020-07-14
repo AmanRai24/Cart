2d3dcc5d09517d8b7e93f56d326e21a8f9725519
Aman Rai <arai24197@gmail.com>
2020-07-14 21:04:21 +0530

    added navbar

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-class CartItem extends React.Component{
+const CartItem=(props)=>{
     
-  
-
-
-    render(){
-        console.log('this.props',this.props);
-        const{price,title,qty}=this.props.product;
-        const{product,onIncreaseQuantity,onDecreaseQuantity,onDeleteProduct}=this.props;
+        
+        const{price,title,qty}=props.product;
+        const{product,onIncreaseQuantity,onDecreaseQuantity,onDeleteProduct}=props;
         return(
             <div className="cart-item">
                 <div className="left-block">
@@ -22,13 +18,12 @@ class CartItem extends React.Component{
                         {/* Buttons*/}
                         <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/992/992651.svg" onClick={()=> onIncreaseQuantity(product)} />
                         <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/1665/1665612.svg" onClick={()=>onDecreaseQuantity(product)} />
-                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1214/1214428.svg" onClick={()=>onDeleteProduct(product)} />
+                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1214/1214428.svg" onClick={()=>onDeleteProduct(product.id)} />
                     </div>
                 </div>
             </div>
         );
     }
-}
 const styles={
     image:{
         height:110,
